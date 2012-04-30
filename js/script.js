f2a6f7de208f625ea4933b504c787cae03cc5708
Ado Matejov <a.matejov@centrum.sk>
2012-04-30 20:58:07 +0200

    added settings, but not implemented yet

--- js/script.js
+++ js/script.js
@@ -2,27 +2,19 @@
 function setHeight (element)
 { 
   var result = document.getElementById("ddg_zeroclick");
-  var h = parseInt(element.style.height);
-  //document.getElementById("search_button").value += h;
+  var h = element.style.height;
+  //document.getElementById("search_button").value = h;
+  
   with (document.body.style)
-	{
-		//width = "500px";
-		//height = h + "px";
-	}
+  {
+  	height = h + "px";
+  }
 }
 
 function nothingFound(query)
 {
     var ddg_result = document.getElementById("ddg_zeroclick");
     if (ddg_result !== null){
-        /*ddg_result.innerHTML = '<div id="nothing_found">' + 
-                                "No zero click results found." + '</div>' + 
-                                '<div id="see_results">' +  
-                                '<img src="../css/imgs/icon_16.png"/></div>' +
-                                '<a id="nothing_found_more" href="https://duckduckgo.com/?q='+
-                                    encodeURIComponent(query)
-                                +'"> See DuckDuckGo results </a>' + 
-                                '</div>';*/
       ddg_result.innerHTML = '<img style="float: left;" src="../css/imgs/icon_16.png"/>' + 
                                 'No zero click results found.' + 
                                 '<a id="nothing_found_more" href="https://duckduckgo.com/?q='+
@@ -398,7 +390,7 @@ window.onload = function(){
   document.getElementById("search_wrapper").onkeyup = function(){
     var key = event.keyCode;
     var el = document.getElementById('search_wrapper');
-    if (key == 13){
+    if (key === 13){
       if (el.value !== ""){
         search(el.value);
       }else
@@ -407,5 +399,119 @@ window.onload = function(){
       return false;
   };
 }
+
+
+
+
+
+
+
+
+
+
+
+/*******************************************************
+********************************************************
+SETTINGS
+********************************************************
+*******************************************************/
+
+
+var bckg = "red";
+
+var colorsArr = new Array ( ["Red (default)", red],
+                            ["Blue", blue],
+                            ["Green", green],
+                            ["Orange", orange],
+                            ["Purple", purple],
+                            ["Light Blue", light_blue],
+                            ["Light Green", light_green ] );
+
+
+function initSettings()	
+{
+    System.Gadget.settingsUI = "settings.html";
+}
+
+function loadSetts()
+{
+  	ddgSets = new GetDDGSettings();	
+	  colors.value = ddgSets.background; 
+    //addOptions();
+	  //setDefaultValue(ddgSets);
+    System.Gadget.onSettingsClosing = CloseSets;
+
+}
+
+function GetDDGSettings()
+{
+    this.background = System.Gadget.Settings.read("background");
+}
+
+function addOptions()
+{   
+    for(var i = 0; i < colorsArr.length; i++){
+		    colors.options[i] = new Option(colorsArr[i][0], colorsArr[i][1]);
+		    colors.options[i].title = colorsArr[i][0];
+	  }
+}
+/*
+function setDefaultValue(ddgSets)
+{
+    switch (ddgSets.background)
+    {
+        case "Red (default)":
+            pass
+        case "Blue":
+            pass
+        case "Green":
+            pass
+        case "Orange":
+            pass
+        case "Purple":
+            pass
+        case "Light Blue":
+            pass
+        case "Light Green":
+            pass
+        default:
+            colors.options[0] = ""
+    }
+}*/
+
+
+function SaveSettings()
+{ 
+    System.Gadget.Settings.writeString("Background", colors.value);
+    ddgSets.background = colors.value;
+	  setSettings();
+}
+
+function setSettings()
+{
+    var bckg = document.getElementById('header');
+    bckg.style.backgroundImage = 'url("css/imgs/bckgs/' + bckg +'.png") repeat-x scroll 0 0 #CD473B';
+    bckg.style.backgroundRepeat = "repeat-x scroll 0 0 #CD473B";
+}
+
+
+//when the Settings are closing
+function CloseSets(event)
+{
+	if (event.closeAction == event.Action.commit){
+		SaveSettings();
+	}else if(event.closeAction == event.Action.cancel){
+	}
+	event.cancel = false;
+}
+
+
+
+
+
+
+
+
+
 
 
