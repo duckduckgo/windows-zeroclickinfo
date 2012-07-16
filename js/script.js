779ab84922b6b55ae666cdf034736f77ee48612a
Ado Matejov <a.matejov@centrum.sk>
2012-07-16 14:10:08 +0200

    magically fixed rounded corners, should looks better

--- js/script.js
+++ js/script.js
@@ -64,21 +64,21 @@ function createResultDiv()
     var ddg_result = document.getElementById("ddg_zeroclick");
     showZeroClick();
     if (ddg_result === null){ 
-        /*var out = '<img src="css/imgs/assets/results_top.png" id="rounded-top" />'
+        var out = '<img src="css/imgs/assets/results_top_new.png" id="rounded-top" />'
+                + '<div id="res"><div id="ddg_zeroclick"></div></div>'
+                + '<img src="css/imgs/assets/results_bot_new.png" id="rounded-bot" />';
+
+        /*var out = '<div style="background-image: url("css/imgs/assets/no-shadow/top_test_no_shadow.png");" id="rounded-top" />'
                 + '<div id="res"><div id="ddg_zeroclick"></div></div>'
                 + '<img src="css/imgs/assets/results_bot.png" id="rounded-bot" />';*/
 
-        /*var out ='<g:background src="css/imgs/assets/results_top.png" id="rounded-top"><div/></g:background>'
-                + '<div id="res"><div id="ddg_zeroclick"></div></div>'
-                +'<g:background src="css/imgs/assets/results_bot.png" id="rounded-bot"><div/></g:background>';*/
-
-        var out = '<g:background src="css/imgs/assets/results_top_new.png" id="roundedTop">'
+        /*var out = '<g:background src="css/imgs/assets/no-shadow/top_test_no_shadow.png" id="roundedTop">'
                 +   '<div></div>'
                 + '</g:background>'
                 + '<div id="res"><div id="ddg_zeroclick"></div></div>'
-                + '<g:background src="css/imgs/assets/results_bot_new.png" id="roundedBot">'
+                + '<g:background src="css/imgs/assets/no-shadow/bot_test_no_shadow.png" id="roundedBot">'
                 +   '<div></div>'
-                + '</g:background>';
+                + '</g:background>';*/
 
         result.innerHTML = out;
         ddg_result = document.getElementById("ddg_zeroclick");
